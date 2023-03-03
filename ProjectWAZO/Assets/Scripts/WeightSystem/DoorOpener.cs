e102e6e880a47252ab6a4c2d72e8ba5e10738d3f
PHILIPPON <m.philippon@rubika-edu.com>
2023-03-03 14:44:30 +0100

    Création Porte

--- ProjectWAZO/Assets/Scripts/WeightSystem/DoorOpener.cs
+++ ProjectWAZO/Assets/Scripts/WeightSystem/DoorOpener.cs
@@ -1,16 +1,20 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace WeightSystem
 {
     public class DoorOpener : Activator
     {
+        [SerializeField] private float openingSpeed = 1f;
         public override void Activate()
         {
+            transform.DOLocalMove(transform.localPosition + Vector3.up * transform.localScale.y, openingSpeed);
             Debug.Log("Door is Open");
         }
 
         public override void Deactivate()
         {
+            transform.DOLocalMove(transform.localPosition - Vector3.up * transform.localScale.y, openingSpeed);
             Debug.Log("Door is Closed");
         }
     }
