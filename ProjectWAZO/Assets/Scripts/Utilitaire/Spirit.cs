da813ab7868a101a74612fecf1502cd3c2ef88b2
PHILIPPON <m.philippon@rubika-edu.com>
2023-04-04 18:58:51 +0200

    New features : all spirit have to wait + move | gather | disperse | disappear behaviour

--- ProjectWAZO/Assets/Scripts/Utilitaire/Spirit.cs
+++ ProjectWAZO/Assets/Scripts/Utilitaire/Spirit.cs
@@ -17,7 +17,7 @@ namespace Utilitaire
         [SerializeField] private NavMeshAgent spiritAgent;
         
         private SpiritEvent _linkedEvent;
-        private bool _isMoving;
+        private bool _isMoving, _waitForNextStep;
         private int _nextPoint;
         
         private void OnTriggerExit(Collider other)
@@ -42,34 +42,87 @@ namespace Utilitaire
 
             if (!_isMoving) return;
             if (TooFar()) return;
-            
-            _nextPoint++;
-            if (_nextPoint < _linkedEvent.waypoints.Length) spiritAgent.destination = _linkedEvent.waypoints[_nextPoint];
-            else
+
+            if(_waitForNextStep) return;
+            if (_linkedEvent.waypoints[_nextPoint].waitBetweenStep != 0)
             {
-                _isMoving = false;
-                Dispersion(false);
+                _waitForNextStep = true;
+                if(_linkedEvent.waypoints[_nextPoint].allSpiritsWait) _linkedEvent.SpiritWait();
             }
+            else NextStep();
         }
         
         //SpiritEvent
-        public void SetDestination(SpiritEvent callingEvent)
+        public void StartEvent(SpiritEvent callingEvent)
         {
             _linkedEvent = callingEvent;
             var waypointAmount = _linkedEvent.waypoints.Length;
             if(waypointAmount==0) Dispersion();
             else
             {
-                if(waypointAmount > 1) ChooseFirstPoint();
-                spiritAgent.destination = _linkedEvent.waypoints[_nextPoint];
+                switch (_linkedEvent.waypoints[0].behaviour)
+                {
+                    case Waypoint.Behaviour.Move:
+                        if(waypointAmount > 1) ChooseFirstPoint();
+                        Movement();
+                        break;
+                    case Waypoint.Behaviour.Gather:
+                        Gathering();
+                        break;
+                    case Waypoint.Behaviour.Disperse:
+                        Dispersion();
+                        break;
+                    case Waypoint.Behaviour.Disappear:
+                        StartCoroutine(Disappearance());
+                        break;
+                    default:
+                        Movement();
+                        throw new ArgumentOutOfRangeException();
+                }
+                
                 _isMoving = true;
             }
         }
 
+        private void NextStep()
+        {
+            _nextPoint++;
+            if (_nextPoint < _linkedEvent.waypoints.Length)
+            {
+                spiritAgent.speed = _linkedEvent.waypoints[_nextPoint].spiritSpeed;
+                switch (_linkedEvent.waypoints[_nextPoint].behaviour)
+                {
+                    case Waypoint.Behaviour.Move:
+                        Movement();
+                        break;
+                    case Waypoint.Behaviour.Gather:
+                        Gathering();
+                        break;
+                    case Waypoint.Behaviour.Disperse:
+                        Dispersion();
+                        break;
+                    case Waypoint.Behaviour.Disappear:
+                        StartCoroutine(Disappearance());
+                        break;
+                    default:
+                        Movement();
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            
+            //After final step
+            else
+            {
+                _isMoving = false;
+                if(_linkedEvent.disappearOnEnd) StartCoroutine(Disappearance());
+                if(_linkedEvent.waypoints[^1].behaviour == Waypoint.Behaviour.Move)Dispersion(false);
+            }
+        }
+        
         private void ChooseFirstPoint()
         {
-            var wpWpDistance = (_linkedEvent.waypoints[0] - _linkedEvent.waypoints[1]).magnitude;
-            var spWpDistance = (transform.position - _linkedEvent.waypoints[1]).magnitude;
+            var wpWpDistance = (_linkedEvent.waypoints[0].position - _linkedEvent.waypoints[1].position).magnitude;
+            var spWpDistance = (transform.position - _linkedEvent.waypoints[1].position).magnitude;
             if (spWpDistance < wpWpDistance + spiritAgent.stoppingDistance) _nextPoint++;
         }
 
@@ -78,6 +131,16 @@ namespace Utilitaire
             return spiritAgent.remainingDistance > spiritAgent.stoppingDistance;
         }
 
+        private void Movement()
+        {
+            spiritAgent.destination = _linkedEvent.waypoints[_nextPoint].position;
+        }
+
+        private void Gathering()
+        {
+            throw new NotImplementedException();
+        }
+        
         private void Dispersion(bool still = true)
         {
             float spreadAngle;
@@ -91,11 +154,22 @@ namespace Utilitaire
             {
                 var rangeAngle = Mathf.Deg2Rad * Vector3.Angle(Vector3.forward, transform.forward);
                 spreadAngle = Random.Range(0f, 1f)*Mathf.PI + rangeAngle;
-                spreadDirection = _linkedEvent.waypoints[^1] + new Vector3(MathF.Cos(spreadAngle), 0, Mathf.Sin(spreadAngle))*10;
+                spreadDirection = _linkedEvent.waypoints[^1].position + new Vector3(MathF.Cos(spreadAngle), 0, Mathf.Sin(spreadAngle))*10;
             }
-            
             spiritAgent.destination = spreadDirection;
-            StartCoroutine(Disappearance());
+        }
+
+        public void AllSpiritStartToWait()
+        {
+            StartCoroutine(WaitBetweenStep());
+        }
+        
+        private IEnumerator WaitBetweenStep()
+        {
+            yield return new WaitForSeconds(_linkedEvent.waypoints[_nextPoint].waitBetweenStep);
+            NextStep();
+            yield return new WaitForEndOfFrame();
+            _waitForNextStep = false;
         }
 
         private IEnumerator Disappearance()
