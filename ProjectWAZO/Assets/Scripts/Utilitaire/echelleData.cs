bfa3b6daf339c5e03ae5c6fa8ac77d514de2ade5
DEHAY <e.dehay@rubika-edu.com>
2023-04-12 12:10:33 +0200

    Intégration des interacteurs de LD

--- ProjectWAZO/Assets/Scripts/Utilitaire/echelleData.cs
+++ ProjectWAZO/Assets/Scripts/Utilitaire/echelleData.cs
@@ -13,6 +13,7 @@ public class echelleData : MonoBehaviour
     }
         
     [SerializeField] public Orientation orientation;
+    public float echelleSpeed = 7;
     private void Update()
     {
         if (Controller.instance.isEchelle && PickUpObjects.instance.currentEchelle == gameObject)
@@ -21,19 +22,19 @@ public class echelleData : MonoBehaviour
             {
                 case Orientation.nord:
                    // Controller.instance.transform.rotation = new Quaternion(-90,0,0,0);
-                    Controller.instance.rb.velocity = new Vector3(0,Controller.instance.moveInput.z,0) * (20 * (Controller.instance.airControlSpeed * Time.deltaTime));
+                    Controller.instance.rb.velocity = new Vector3(0,Controller.instance.moveInput.z,0) * (echelleSpeed * (Controller.instance.airControlSpeed * Time.deltaTime));
                     break;
                 case Orientation.sud:
                    // Controller.instance.transform.rotation = new Quaternion(-90,180,0,0);
-                    Controller.instance.rb.velocity = new Vector3(0,-Controller.instance.moveInput.z,0) * (20 * (Controller.instance.airControlSpeed * Time.deltaTime));
+                    Controller.instance.rb.velocity = new Vector3(0,-Controller.instance.moveInput.z,0) * (echelleSpeed * (Controller.instance.airControlSpeed * Time.deltaTime));
                     break;
                 case Orientation.est:
                    // Controller.instance.transform.rotation = new Quaternion(-90,90,0,0);
-                    Controller.instance.rb.velocity = new Vector3(0,Controller.instance.moveInput.x,0) * (20 * (Controller.instance.airControlSpeed * Time.deltaTime));
+                    Controller.instance.rb.velocity = new Vector3(0,Controller.instance.moveInput.x,0) * (echelleSpeed * (Controller.instance.airControlSpeed * Time.deltaTime));
                     break;
                 case Orientation.ouest:
                  //   Controller.instance.transform.rotation = new Quaternion(-90,-90,0,0);
-                    Controller.instance.rb.velocity = new Vector3(0,-Controller.instance.moveInput.x,0) * (20 * (Controller.instance.airControlSpeed * Time.deltaTime));
+                    Controller.instance.rb.velocity = new Vector3(0,-Controller.instance.moveInput.x,0) * (echelleSpeed * (Controller.instance.airControlSpeed * Time.deltaTime));
                     break;
             }
         }
