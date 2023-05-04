b23e44d1fab03e0ca2773482a7e5f081396468ec
Lenox <>
2023-05-04 20:17:35 +0200

    vfxorb

---
+++ ProjectWAZO/Assets/VFX/VFX_orb/Stopit.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stopit : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem vfxreplacing;
+    [SerializeField] private ParticleSystem basic;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            vfxreplacing.Play();
+            basic.Stop();
+   
+        }
+    }
+}
