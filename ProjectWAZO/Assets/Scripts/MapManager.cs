38fc1cc18490414671a38a276b38450d237e6132
Ethan Dehay <e.dehay@rubika-edu.com>
2023-05-14 18:18:57 +0200

    fIx clé

--- ProjectWAZO/Assets/Scripts/MapManager.cs
+++ ProjectWAZO/Assets/Scripts/MapManager.cs
@@ -52,7 +52,7 @@ public class MapManager : MonoBehaviour
     IEnumerator ShowAndHide(float duration)
     {
         iconMapUpdate.gameObject.SetActive(true);
-        iconMapUpdate.DOAnchorPosX(115, 0.5f);
+        iconMapUpdate.DOAnchorPosX(-20, 0.5f);
         yield return new WaitForSeconds(duration);
         iconMapUpdate.DOAnchorPosX(350, 0.5f).OnComplete((() =>   iconMapUpdate.gameObject.SetActive(false)));
       
