59a63345daee20aa095e2ad10414a37a8b482799
Ethan Dehay <e.dehay@rubika-edu.com>
2023-05-14 19:21:18 +0200

    Plein d'options !

--- ProjectWAZO/Assets/Scripts/PauseMenu.cs
+++ ProjectWAZO/Assets/Scripts/PauseMenu.cs
@@ -1,9 +1,11 @@
-using System;
+    using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
-using UnityEngine;
-using UnityEngine.UIElements;
+    using TMPro;
+    using UnityEngine;
+    using UnityEngine.Audio;
+    using UnityEngine.UIElements;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -14,6 +16,11 @@ public class PauseMenu : MonoBehaviour
     private CanvasGroup CG;
     public CanvasGroup canvasOptions;
     public static PauseMenu instance;
+
+    [Header("Options")] 
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+    public AudioMixer mixer;
     
     public void Start()
     {
@@ -21,8 +28,27 @@ public class PauseMenu : MonoBehaviour
         {
             instance = this;
         }
-        
         CG = GetComponent<CanvasGroup>();
+        
+        // Pour les résolutions
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width 
+                && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
     public void PauseUnPause()
@@ -34,7 +60,7 @@ public class PauseMenu : MonoBehaviour
             CG.blocksRaycasts = true;
             CG.DOFade(1, 0.5f);
             eventSystem.SetSelectedGameObject(boutonReprendre);
-            CinématiqueManager.instance.isCinématique = false;
+            CinématiqueManager.instance.isCinématique = true;
             Controller.instance.canMove = false;
             Controller.instance.canJump = false;
         }
@@ -45,7 +71,7 @@ public class PauseMenu : MonoBehaviour
             CG.blocksRaycasts = false;
             CG.DOFade(0, 0.5f);
             eventSystem.SetSelectedGameObject(null);
-            CinématiqueManager.instance.isCinématique = true;
+            CinématiqueManager.instance.isCinématique = false;
             Controller.instance.canMove = true;
             Controller.instance.canJump = true;
         }
@@ -75,9 +101,59 @@ public class PauseMenu : MonoBehaviour
         canvasOptions.blocksRaycasts = false;
         canvasOptions.DOFade(0, 0.5f);
     }
-
+    
     public void Quit()
     {
         Application.Quit();
+    }
+    
+    
+    
+    //---------------------------Fonction pour les Options-----------------------------------
+
+    public void ChangeQuality(int index)
+    {
+        if (index == 0)
+        {
+            QualitySettings.SetQualityLevel(2,true);   
+        }
+        else if (index == 1)
+        {
+            QualitySettings.SetQualityLevel(1,true);   
+        }
+        else if (index == 2)
+        {
+            QualitySettings.SetQualityLevel(0,true);   
+        }
+    }
+
+    public void SetMasterLevel(float sliderValue)
+    {
+       // mixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) *40);
+       mixer.SetFloat("MasterVol", (-80 + sliderValue*100));
+    }
+    
+    public void SetMusicLevel(float sliderValue)
+    {
+        //mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) *40);
+        mixer.SetFloat("MasterVol", (-80 + sliderValue*100));
+    }
+    
+    public void SetSFXLevel(float sliderValue)
+    {
+        //mixer.SetFloat("SFXVol", Mathf.Log10(sliderValue) *40);
+        mixer.SetFloat("MasterVol", (-80 + sliderValue*100));
+    }
+    
+    public void ChangeFullScreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+        Debug.Log("changement fullscrren");
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
