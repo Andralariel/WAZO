6d9102804532023b93dedf5b88756ed460cbf9aa
PHILIPPON <m.philippon@rubika-edu.com>
2023-05-19 17:41:03 +0200

    Cut-Out Object -> With object transparency

--- ProjectWAZO/Assets/Scripts/TechArt/CutOutObjects.cs
+++ ProjectWAZO/Assets/Scripts/TechArt/CutOutObjects.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace TechArt
@@ -11,20 +12,37 @@ namespace TechArt
         [SerializeField] private float detectionRadius;
         [SerializeField] private Camera mainCamera;
         
-        void Update()
+        // void Update()
+        // {
+        //     var offset = player.position - transform.position;
+        //     for (int i = 0; i < mats.Length; i++)
+        //     {
+        //         if (Physics.SphereCast(transform.position, detectionRadius, offset,out RaycastHit hit,offset.magnitude,maskToTarget))
+        //         {
+        //             mats[i].SetFloat("_CutoutSize",size);
+        //         }
+        //         else mats[i].SetFloat("_CutoutSize",0);
+        //     
+        //         var view = mainCamera.WorldToViewportPoint(player.position);
+        //         mats[i].SetVector("_CutoutPosition", view);
+        //     }
+        // }
+
+        [SerializeField] private Shader mainShader;
+        [SerializeField] private Shader seeThroughShader;
+
+        private void OnTriggerEnter(Collider other)
         {
-            var offset = player.position - transform.position;
-            for (int i = 0; i < mats.Length; i++)
-            {
-                if (Physics.SphereCast(transform.position, detectionRadius, offset,out RaycastHit hit,offset.magnitude,maskToTarget))
-                {
-                    mats[i].SetFloat("_CutoutSize",size);
-                }
-                else mats[i].SetFloat("_CutoutSize",0);
-            
-                var view = mainCamera.WorldToViewportPoint(player.position);
-                mats[i].SetVector("_CutoutPosition", view);
-            }
+            if (other.gameObject.layer != 3) return; //layer 3 = ground
+            Debug.Log(other.gameObject.name + " is transparent");
+            other.GetComponent<MeshRenderer>().material.shader = seeThroughShader;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.layer != 3) return; //layer 3 = ground
+            Debug.Log(other.gameObject.name + " is opaque");
+            other.GetComponent<MeshRenderer>().material.shader = mainShader;
         }
     }
 }
