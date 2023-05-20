e741d14d47799e6000e9f2ec0b5e1c03030937f7
Lenox <>
2023-05-20 16:35:56 +0200

    Beacons

--- ProjectWAZO/Assets/Scripts/TechArt/InteractiveGrass.cs
+++ ProjectWAZO/Assets/Scripts/TechArt/InteractiveGrass.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 namespace TechArt
 {
     public class InteractiveGrass : MonoBehaviour
     {
         [SerializeField] Material[] mats;
+        [SerializeField] VisualEffect[] beacons;
         [SerializeField] private GameObject character;
 
         // Update is called once per frame
@@ -14,6 +16,13 @@ namespace TechArt
             {
                 mats[i].SetVector("_Characterpos", character.transform.position);
             }
+            
+            for (int i = 0; i < beacons.Length; i++)
+            {
+                float distance = Vector3.Distance (character.transform.position, new Vector3(beacons[i].transform.position.x,beacons[i].transform.position.y-6,beacons[i].transform.position.z));
+                Debug.Log(distance);
+                beacons[i].SetFloat("Distance_Joueur", distance);
+            }
         }
 
         void OnDestroy()
@@ -21,6 +30,10 @@ namespace TechArt
             for (int i = 0; i < mats.Length; i++)
             {
                 mats[i].SetVector("_Characterpos", new Vector3(0,0,0));
+            }
+            for (int i = 0; i < beacons.Length; i++)
+            {
+                beacons[i].SetVector3("Distance_Joueur", new Vector3(0,0,0));
             }
         }
     }
