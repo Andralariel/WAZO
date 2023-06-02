40c71c6074144d0a3f4c69a1ab420255b6e7726d
PHILIPPON <m.philippon@rubika-edu.com>
2023-06-02 11:30:51 +0200

    Fix FloatingRock

--- ProjectWAZO/Assets/Scripts/Interaction/FloatingRock.cs
+++ ProjectWAZO/Assets/Scripts/Interaction/FloatingRock.cs
@@ -1,3 +1,4 @@
+using _3C;
 using DG.Tweening;
 using UnityEngine;
 
@@ -10,23 +11,40 @@ namespace Interaction
         
         private bool _birdOnRock;
         private Tweener _currentTween;
+
+        private void Awake()
+        {
+            _inputAction = new PlayerControls();
+            _inputAction.Player.Jump.performed += ctx => JumpOut();
+        }
         
+        private PlayerControls _inputAction;
+        private void OnEnable()
+        {
+            _inputAction.Player.Enable();
+        }
+        private void OnDisable()
+        {
+            _inputAction.Player.Disable();
+        }
+
         private void OnCollisionEnter(Collision collision)
         {
             if (collision.gameObject.layer != 6) return;
             if (_birdOnRock) return;
 
             _birdOnRock = true;
+            Controller.instance.transform.parent = transform;
             _currentTween?.Kill();
             _currentTween = transform.DOMove(transform.position - new Vector3(0, heightToMove, 0), moveDuration);
         }
 
-        private void OnCollisionExit(Collision other)
+        private void JumpOut()
         {
-            if (other.gameObject.layer != 6) return;
             if (!_birdOnRock) return;
 
             _birdOnRock = false;
+            Controller.instance.transform.parent = null;
             _currentTween?.Kill();
             _currentTween = transform.DOMove(transform.position + new Vector3(0, heightToMove, 0), moveDuration);
         }
