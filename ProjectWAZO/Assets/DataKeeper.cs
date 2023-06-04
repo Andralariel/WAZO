dce2dc1ca8e394cd24251f4f3b46966c90a5896f
PHILIPPON <m.philippon@rubika-edu.com>
2023-06-04 16:08:37 +0200

    Changed Cinematic DataKeeper to the prefab one

--- ProjectWAZO/Assets/DataKeeper.cs
+++ ProjectWAZO/Assets/DataKeeper.cs
@@ -6,15 +6,7 @@ public class DataKeeper : MonoBehaviour
     public static DataKeeper instance;
     public bool isHat;
     public GameObject prefabHat;
-    
-    /*public bool isFullScreen;
-    public bool isUI;
-    public int currentQuality;
-    public int currentResolution;
-    public float currentVolume;
-    public float currentMusic;
-    public float currentSFX;*/
-    
+
     void Awake()
     {
         if (instance != default && instance!=this)
@@ -31,7 +23,6 @@ public class DataKeeper : MonoBehaviour
     {
         if (isHat)
         {
-            Debug.Log("STAAAAAR §§§§");
             GameObject hat = Instantiate(prefabHat, Controller.instance.transform.position, Quaternion.identity);
             hat.GetComponent<HatManager>().PutHat();
         }
